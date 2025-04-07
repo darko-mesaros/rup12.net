73de357464aac481ee3b21f74b7aa079ec8051f5
darko-mesaros <darko.subotica@gmail.com>
2025-04-06 21:57:19 -0700

    Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,53 +5,49 @@ import vercelStatic from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import awsAmplify from 'astro-aws-amplify';
+
+import cloudflare from '@astrojs/cloudflare';
 
 const options = {
-	// Specify the theme to use or a custom theme json, in our case
-	// it will be a moonlight-II theme from
-	// https://github.com/atomiks/moonlight-vscode-theme/blob/master/src/moonlight-ii.json
-	// Callbacks to customize the output of the nodes
-	//theme: json,
-	onVisitLine(node) {
-		// Prevent lines from collapsing in `display: grid` mode, and
-		// allow empty lines to be copy/pasted
-		if (node.children.length === 0) {
-			node.children = [
-				{
-					type: 'text',
-					value: ' '
-				}
-			];
-		}
-	},
-	onVisitHighlightedLine(node) {
-		// Adding a class to the highlighted line
-		node.properties.className = ['highlighted'];
-	}
+    // Specify the theme to use or a custom theme json, in our case
+    // it will be a moonlight-II theme from
+    // https://github.com/atomiks/moonlight-vscode-theme/blob/master/src/moonlight-ii.json
+    // Callbacks to customize the output of the nodes
+    //theme: json,
+    onVisitLine(node) {
+        // Prevent lines from collapsing in `display: grid` mode, and
+        // allow empty lines to be copy/pasted
+        if (node.children.length === 0) {
+            node.children = [
+                {
+                    type: 'text',
+                    value: ' '
+                }
+            ];
+        }
+    },
+    onVisitHighlightedLine(node) {
+        // Adding a class to the highlighted line
+        node.properties.className = ['highlighted'];
+    }
 };
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jugokozmos.net',
 
-	markdown: {
-		syntaxHighlight: false,
-		// Disable syntax built-in syntax hightlighting from astro
-		rehypePlugins: [[rehypePrettyCode, options]],
-		remarkPlugins: [remarkReadingTime]
-	},
+    markdown: {
+        syntaxHighlight: false,
+        // Disable syntax built-in syntax hightlighting from astro
+        rehypePlugins: [[rehypePrettyCode, options]],
+        remarkPlugins: [remarkReadingTime]
+    },
 
-	integrations: [react(), sitemap()],
-	output: 'static',
+    integrations: [react(), sitemap()],
+    output: 'static',
 
-	adapter: vercelStatic({
-		webAnalytics: {
-			enabled: true
-		}
-	}),
-  adapter: awsAmplify(),
-	vite: {
-		plugins: [tailwindcss()]
-	}
+    adapter: cloudflare(),
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
